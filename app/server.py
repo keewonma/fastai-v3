a561f53a567ed58ae4405a688e37cc76e291fedf
Jonathan Ma <jonathankma@gmail.com>
2019-04-03 14:08:41 -0700

    Update server.py
    removed upper and img_data from line 79/80

--- app/server.py
+++ app/server.py
@@ -77,7 +77,7 @@ async def analyze(request):
              <p>Confidence: %s</p>
            </body>
         </html>
-    """ %(pred_class.upper(), pred_probs, img_data))
+    """ %(pred_class, pred_probs))
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app=app, host='0.0.0.0', port=5042)

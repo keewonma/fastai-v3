79073a3447cd632ff72e5c69162ddf37d224bcc5
Jonathan Ma <jonathankma@gmail.com>
2019-04-03 13:15:19 -0700

    Update server.py
    testing multiple object outputs JSONResponse

--- app/server.py
+++ app/server.py
@@ -64,7 +64,14 @@ async def analyze(request):
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
     outputs = learn.predict(img)[2]
-    return JSONResponse({'result': str(prediction), 'probabilities': list(zip(classes, ((outputs *100).tolist())))})
+    return JSONResponse({'result': [
+                                         {
+                                             'prediction'   : str(prediction), 
+                                             'test'         : str(test),
+                                             'probabilities': list(zip(classes, ((outputs *100).tolist())))
+                                         }
+                                     [
+                         })
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app=app, host='0.0.0.0', port=5042)

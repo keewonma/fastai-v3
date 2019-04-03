777b7ef18c7d6686f2effa2faa8f792a93eff6e0
Jonathan Ma <jonathankma@gmail.com>
2019-04-03 14:58:45 -0700

    Update server.py
        data = {
                'Prediction':pred_class,
                'Confidence': pred_probs
                }
        return JSONResponse(data)

--- app/server.py
+++ app/server.py
@@ -69,15 +69,11 @@ async def analyze(request):
             key=lambda p: p[1],
             reverse=True
         )
-    return HTMLResponse(
-        """
-        <html>
-           <body>
-             <p>Prediction: <b>%s</b></p>
-             <p>Confidence: %s</p>
-           </body>
-        </html>
-    """ %(pred_class, pred_probs))
+    data = {
+            'Prediction':pred_class,
+            'Confidence': pred_probs
+            }
+    return JSONResponse(data)
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app=app, host='0.0.0.0', port=5042)

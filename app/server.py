a271ec0fbf8af977bd0865f00b0063a10c8a6d6b
Jonathan Ma <jonathankma@gmail.com>
2019-04-04 16:34:32 -0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -73,8 +73,8 @@ async def analyze(request):
     #probabilities1 = {'Probability Top 1': str(pred_probs[0])}
     #probabilities2 = {'Probability Top 2': str(pred_probs[1])}
     #probabilities3 = {'Probability Top 3': str(pred_probs[2])}
-    data = "Prediction: %s/nProbability Top 1: %s/nProbability Top 2:%s/n Probability Top 3: %s"%(pred_class, pred_probs[0],pred_probs[1], pred_probs[2])
+    data = "%s/nProbability Top 1: %s/nProbability Top 2:%s/n Probability Top 3: %s"%(pred_class, pred_probs[0],pred_probs[1], pred_probs[2])
 
-    return JSONResponse('result':pred_class)
+    return JSONResponse({'Results': str(data)})
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app=app, host='0.0.0.0', port=5042)
